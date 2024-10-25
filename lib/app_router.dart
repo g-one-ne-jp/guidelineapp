678b848c0b199d36e7063f0e2f33034086bbc870
Masayuki Furuyama <xl1200n.09@gmail.com>
2024-10-25 14:55:30 +0900

    Update:保存したPDFをStorageに書き込み

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_template/ui/page/function/function_page_pdfeditt.dart';
 import 'package:flutter_template/ui/page/home/tab/ui_page_home_catalog_tab_bookmark.dart';
 import 'package:flutter_template/ui/page/home/tab/ui_page_home_catalog_tab_home.dart';
 import 'package:flutter_template/ui/page/home/tab/ui_page_home_catalog_tab_home_minor.dart';
