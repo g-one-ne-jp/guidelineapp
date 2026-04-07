b5f640acf70887db370e83affc458723bcdc73be
Kazuki Naito <kato.kinako@gmail.com>
2026-04-07 14:39:08 +0900

    pdfライブラリで使っているNSLocalNetworkUsageDescriptionをinfo.plistに追加

--- shell/ios_profile.sh
+++ shell/ios_profile.sh
@@ -5,5 +5,8 @@ sh ./shell/firebase/firebase_release.sh
 
 #リリースビルド
 flutter build ipa --profile --export-options-plist="./ios/configs/dev/ExportOptions.plist" --build-name=${VERSION_NAME}-profile --build-number=${VERSION_CODE_PROFILE}
+# リネームしたアーカイブ、IPAをarchiveディレクトリに移動
+# ./archiveは通常は存在しないディレクトリなので、作成しなければならない。
+mkdir -p ./archive
 mv ./build/ios/archive/Runner.xcarchive ./archive/Runner-P-${VERSION_NAME}-${VERSION_CODE_PROFILE}.xcarchive
 mv ./build/ios/ipa/Apps/JCSGuidelines.ipa ./archive/Runner-P-${VERSION_NAME}-${VERSION_CODE_PROFILE}.ipa
--- shell/ios_relase.sh
+++ shell/ios_relase.sh
@@ -6,5 +6,8 @@ sh ./shell/firebase/firebase_release.sh
 
 #リリースビルド
 flutter build ipa --release --export-options-plist="./ios/configs/ExportOptions.plist" --build-name=${VERSION_NAME}-release --build-number=${VERSION_CODE}
+# リネームしたアーカイブ、IPAをarchiveディレクトリに移動
+# ./archiveは通常は存在しないディレクトリなので、作成しなければならない。
+mkdir -p ./archive
 mv ./build/ios/archive/Runner.xcarchive ./archive/Runner-R-${VERSION_NAME}-${VERSION_CODE}.xcarchive
 mv ./build/ios/ipa/JCSGuidelines.ipa ./archive/Runner-R-${VERSION_NAME}-${VERSION_CODE}.ipa
